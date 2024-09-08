c986d646346bc812a8014a6f2026370ed2d743bf
Samir Shuman <samirshuman@gmail.com>
2024-09-07 21:45:33 -0400

    update login and registration

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS=[
     'localhost','https://samirshuman-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
     ]
 CSRF_TRUSTED_ORIGINS=[
-    'https://samirshuman-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://samirshuman-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://samirshuman-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
     ]
 
 REST_FRAMEWORK = {
@@ -66,7 +67,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static')
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -141,6 +144,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
