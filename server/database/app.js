46a3434a5d74b00d5516c13956e7217e13c3a655
Samir Shuman <samirshuman@gmail.com>
2024-09-09 03:02:15 -0400

    created API endpoints in app.js

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,38 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+try {
+    // Extract the state parameter from the request URL
+    const state = req.params.state;
+    const documents = await Dealerships.find({ state: state });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+try {
+    // Extract the state parameter from the request URL
+    const id = parseInt(req.params.id);
+    const documents = await Dealerships.findOne({ id: id }); // Query by the 'id' field
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 //Express route to insert review
